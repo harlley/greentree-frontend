5f68dc0d3c4661a3e94769395e05b7400ac18695
Harlley Oliveira <harlley@gmail.com>
2019-08-20 15:17:05 +0100

    Add Google Map prototype using current localization

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,36 @@
 <template>
   <div id="app">
-    <h1>frontend</h1>
+    <gmap-map id="map" :center="center" :zoom="18" style="width: 100%; height: 300px">
+      <gmap-marker :key="index" v-for="(m, index) in markers" :position="center = m.position"></gmap-marker>
+    </gmap-map>
   </div>
 </template>
+
+<script>
+    export default{
+        data(){
+          return {
+            center: {lat: 0, lng: 0},
+            markers: [
+              {position: {lat: 0.0, lng: 0.0}}
+            ],
+            getMap: this.$root.mapping
+          }
+        },
+
+        mounted(){
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+              let pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude
+              };
+              this.markers[0].position.lat = pos.lat;
+              this.markers[0].position.lng = pos.lng;
+              console.log(pos);
+            }.bind(this));
+          }
+        },
+        
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBfMhFCkVNku0hQ6ddw8MWoQgOLU3Hnyks'
+  },
+  installComponents: true
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
