7d6d4b0911df35950c10d5baf143dea4e761cab8
Harlley Oliveira <harlley@gmail.com>
2019-08-21 22:50:31 +0100

    add app-menu component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <template>
   <div id="app">
-    <b-navbar fixed-top >
-        <template slot="brand">
-            <b-navbar-item href="/">
-                <img src="@/assets/logo.png">
-            </b-navbar-item>
-        </template>
-        <template slot="start">
-            <b-navbar-item href="/reports/new">
-              <span class="icon">
-                <i class="fas fa-cannabis"></i>
-              </span>             
-               &nbsp;Report Tree
-            </b-navbar-item>
-            <b-navbar-item href="/reports">
-              <span class="icon">
-                <i class="fas fa-table"></i>
-              </span>
-               &nbsp;List Reports
-            </b-navbar-item>
-        </template>
-    </b-navbar>    
+    <app-menu></app-menu>
     <div class="container">
       <router-view/>
     </div>
   </div>
 </template>
+
+<script>
+import AppMenu from './components/AppMenu'
+
+export default {
+  components: {
+    AppMenu
+  } 
+}
+</script>
 
 <style>
 .container {
---
+++ src/components/AppMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar fixed-top >
+      <template slot="brand">
+          <b-navbar-item href="/">
+              <img src="@/assets/logo.png">
+          </b-navbar-item>
+      </template>
+      <template slot="start">
+          <b-navbar-item href="/reports/new">
+            <span class="icon">
+              <i class="fas fa-tree"></i>
+            </span>             
+              &nbsp;Report Tree
+          </b-navbar-item>
+          <b-navbar-item href="/reports">
+            <span class="icon">
+              <i class="fas fa-table"></i>
+            </span>
+              &nbsp;List Reports
+          </b-navbar-item>
+      </template>
+  </b-navbar>
+</template>
